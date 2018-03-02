6fee167b1c37ddbf3cb8a5f2819defc348034c51
Diego Rodriguez <diefrodriguezcha@unal.edu.co>
2018-03-02 07:29:23 -0500

    C3: Section 'Displaying list' completed.

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,15 +8,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
+  /*hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
+  };*/
 
   heroes = HEROES;
 
-  constructor() {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
+  constructor() {
   }
 
   ngOnInit(){
--- angular-tour-of-heroes/src/app/mock-heroes.ts
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,4 +1,4 @@
-import { Hero } form './hero';
+import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
